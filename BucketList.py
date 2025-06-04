23e8d468ca8715bc6b8024ff89e3e9825ef0683d
Jax Lehmann <jaxlehmann@gmail.com>
2025-06-03 22:32:18 -0400

    Add files via upload
    Codedex's Python bucket list!

---
+++ BucketList.py
@@ -0,0 +1,10 @@
+things_to_do = [
+  '🏂 Learn how to snowboard',
+  '🖥️ Work for Google',
+  '👪 Make my parents proud',
+  '💰 Have a sustainable income',
+  '😎 Do cool things',
+  '⚙️ Build confidence in myself'
+]
+for i in things_to_do:
+  print(i)
