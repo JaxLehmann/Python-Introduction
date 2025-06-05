860b05a1b32615766da039fe90bbe2a4e31b08f4
Jax Lehmann <jaxlehmann@gmail.com>
2025-06-05 18:10:49 -0400

    Add files via upload

---
+++ Casses_with_restaurants.py
@@ -0,0 +1,17 @@
+bobs_burgers = Restaurant()
+bobs_burgers.name = "Bob's Burgers"
+bobs_burgers.category = 'American Diner'
+bobs_burgers.rating = 4.7
+bobs_burgers.delivery = False
+
+wendys = Restaurant()
+wendys.name = "Wendy's"
+wendys.category = "Fast Food"
+wendys.rating = 4.6
+wendys.delivery = False
+
+SBD = Restaurant()
+SBD.name = 'Saddle Brook Diner'
+SBD.category = 'American Diner'
+SBD.rating = 4.8
+SBD.delivery = True
