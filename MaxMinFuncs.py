e4284c81953a6169aed35531d8265ebf44d41a4a
Jax Lehmann <jaxlehmann@gmail.com>
2025-06-07 00:26:33 -0400

    Add files via upload

---
+++ MaxMinFuncs.py
@@ -0,0 +1,11 @@
+# Write code below 💖
+# len() gives total length of list
+# max() gives maximum value in list
+# min() gives lowest value in list
+
+lego_parts = [8980, 7323, 5343, 82700, 92232, 1203, 7319, 8903, 2328, 1279, 679, 589]
+shelf_low = min(lego_parts)
+shelf_overstock = max(lego_parts)
+
+print(shelf_low)
+print(shelf_overstock)
