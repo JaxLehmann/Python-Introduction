b45a41e3830164afdbad718e1a21d98c76d26f26
Jax Lehmann <jaxlehmann@gmail.com>
2025-06-08 23:46:55 -0400

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,20 @@
+def addition(a, b):
+  return a + b
+
+def subtraction(a, b):
+  return a - b
+
+def multiplication(a, b):
+  return a * b
+
+def division(a, b):
+  return a / b
+
+def exp(a, b):
+  return a ** b
+
+print(addition(5, 5))
+print(subtraction(4, 1))
+print(multiplication(3, 3))
+print(division(10, 5))
+print(exp(2, 2))
