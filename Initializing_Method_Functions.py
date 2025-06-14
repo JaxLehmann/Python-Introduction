e8532f26d2c9bf925dce86bd5fccf5464d52cc31
Jax Lehmann <jaxlehmann@gmail.com>
2025-06-14 13:14:26 -0400

    Add files via upload

---
+++ Initializing_Method_Functions.py
@@ -0,0 +1,12 @@
+# Write code below 💖
+class City:
+  def __init__(self, name, country, population, landmarks, nickname, mayor):
+    self.name = name
+    self.country = country
+    self.population = population
+    self.landmarks = landmarks
+    self.nickname = nickname
+    self.mayor = mayor
+
+Saddle_Brook = City("Saddle Brook", "USA", 5000, "Devil's Drop", "SB", "Bloomberg")
+LA = City("Los Angelas", "USA", 30000, "Banana Republic", "LA", "Charleston")
