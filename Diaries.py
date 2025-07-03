a7757079c10b8d5ca929544236e7a5bd17142e61
Jax Lehmann <jaxlehmann@gmail.com>
2025-07-03 15:19:03 -0400

    Add files via upload

---
+++ Diaries.py
@@ -0,0 +1,7 @@
+with open('diaries.txt', 'w') as file:
+    file.write ('Dear Diary: \n' 
+            'I really want to become great at coding \n' 
+            'I want to be consistent at the gym \n'
+            'I want to drive a Toyota Corolla when I am older \n'
+            'I want to own a home gym... \n'
+            'July 3rd, 2025')
